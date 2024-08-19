6e20af17d891418175d63f505e4ceaac5c214f2c
Alex Widener <awidener3@gmail.com>
2024-08-19 15:19:11 -0400

    save progress

--- semanticForms.js
+++ semanticForms.js
@@ -2,91 +2,117 @@ window.semanticForms = () => {
   // do some feature detection so none of the JS executes if the browser is too old
   if (typeof document.getElementsByClassName !== 'function' || typeof document.querySelector !== 'function' || !document.body.classList || !window.MutationObserver) return
 
+  const nodeNameLookup = ['TEXTAREA', 'SELECT']
+  const typeLookup = ['checkbox', 'color', 'date', 'datetime-local', 'email', 'file', 'image', 'month', 'number', 'password', 'radio', 'range', 'search', 'tel', 'text', 'time', 'url', 'week']
+
   // progressively enhance form elements that have the semanticForms class
   const forms = document.querySelectorAll('form.semanticForms:not(.semanticFormsActive)')
   for (const form of forms) {
     form.classList.add('semanticFormsActive')
-    if (!form.classList.contains('lowFlow')) {
-      const clearfieldHorizontalOffset = parseInt(form.getAttribute('data-clearfield-horizontal-offset')) || 21
-      const clearfieldVerticalOffset = parseInt(form.getAttribute('data-clearfield-vertical-offset')) || 5
-      const inputs = Array.prototype.slice.call(form.getElementsByTagName('input')).concat(Array.prototype.slice.call(form.getElementsByTagName('textarea'))).concat(Array.prototype.slice.call(form.getElementsByTagName('select')))
-      for (const input of inputs) {
-        if (input.classList.contains('semanticform')) continue
-        if (input.id) {
-          const nodeName = input.nodeName
-          const type = input.getAttribute('type')
-          if (nodeName === 'TEXTAREA' || nodeName === 'SELECT' || type === 'checkbox' || type === 'color' || type === 'date' || type === 'datetime-local' || type === 'email' || type === 'file' || type === 'image' || type === 'month' || type === 'number' || type === 'password' || type === 'radio' || type === 'range' || type === 'search' || type === 'tel' || type === 'text' || type === 'time' || type === 'url' || type === 'week') {
-            let dl = input.parentNode
-            while (dl && dl.nodeName !== 'DL') dl = dl.parentNode
-            if (dl) {
-              if (!dl.classList.contains('floatLabelForm')) dl.classList.add('floatLabelForm')
-              let label
-              if (input.parentNode.parentNode.id && (type === 'checkbox' || type === 'radio')) label = document.querySelector('label[data-for=' + input.parentNode.parentNode.id.replace(/\./g, '\\.') + ']')
-              else label = document.querySelector('label[for=' + input.id.replace(/\./g, '\\.') + ']')
-              input.classList.add('semanticform')
-              if (type === 'checkbox' || type === 'radio') {
-                dl = input.parentNode
-                while (dl && dl.nodeName !== 'DD') dl = dl.parentNode
-                if (dl.firstChild.nodeName !== 'LABEL') {
-                  const newLabel = document.createElement('label')
-                  newLabel.className = 'floatLabelFormAnimatedLabel'
-                  if (type === 'checkbox' && input.parentNode.nodeName === 'DD') {
-                    newLabel.setAttribute('for', input.id)
-                    input.parentNode.classList.add('singleCheckbox')
-                    newLabel.className = ''
-                  }
-                  newLabel.innerHTML = label.innerHTML
-                  dl.insertBefore(newLabel, dl.firstChild)
-                }
-              } else {
-                const newLabel = document.createElement('label')
-                newLabel.setAttribute('for', input.id)
-                newLabel.className = 'floatLabelFormAnimatedLabel'
-                newLabel.innerHTML = label.innerHTML
-                insertAfter(newLabel, input)
-                label.setAttribute('hidden', 'hidden')
+    if (form.classList.contains('lowFlow')) continue
+
+    const clearfieldHorizontalOffset = parseInt(form.getAttribute('data-clearfield-horizontal-offset')) || 21
+    const clearfieldVerticalOffset = parseInt(form.getAttribute('data-clearfield-vertical-offset')) || 5
+
+    // collect all form elements
+    const inputs = Array.from(form.querySelectorAll('input, textarea, select'))
+
+    for (const input of inputs) {
+      // check if input has already been formatted
+      if (input.classList.contains('semanticform') || !input.id) continue
+
+      const type = input.getAttribute('type')
+
+      if (nodeNameLookup.includes(input.nodeName) || typeLookup.includes(type)) {
+        const dl = input.closest('dl')
+        dl.classList.toggle('floatLabelForm', true)
+
+        let label
+
+        if (input.parentNode.parentNode.id && (type === 'checkbox' || type === 'radio')) {
+          label = document.querySelector('label[data-for=' + input.parentNode.parentNode.id.replace(/\./g, '\\.') + ']')
+        } else {
+          label = document.querySelector('label[for=' + input.id.replace(/\./g, '\\.') + ']')
+        }
+
+        input.classList.add('semanticform')
+
+        // checkboxes and radios
+        if (type === 'checkbox' || type === 'radio') {
+          const dd = input.closest('dd')
+          if (dd.firstChild.nodeName !== 'LABEL') {
+            const newLabel = document.createElement('label')
+            newLabel.className = 'floatLabelFormAnimatedLabel'
+
+            if (type === 'checkbox' && input.parentNode.nodeName === 'DD') {
+              newLabel.setAttribute('for', input.id)
+              input.parentNode.classList.add('singleCheckbox')
+              newLabel.className = ''
+            }
+
+            newLabel.innerHTML = label.innerHTML
+            dd.append(newLabel)
+          }
+
+          const div = document.createElement('div')
+          // removes old div that a radio or checkbox may have been added to
+          if (dd.parentElement.nodeName === 'DIV') {
+            dd.parentElement.remove()
+          }
+          div.append(label.closest('dt'), dd)
+          dl.append(div)
+        } else {
+          const newLabel = document.createElement('label')
+          newLabel.setAttribute('for', input.id)
+          newLabel.className = 'floatLabelFormAnimatedLabel'
+          newLabel.innerHTML = label.innerHTML
+          label.setAttribute('hidden', 'hidden')
+          insertAfter(newLabel, input)
+          // label.classList.add('floatLabelFormAnimatedLabel')
+        }
+
+        // standard inputs
+        if (input.nodeName !== 'SELECT' && type !== 'checkbox' && type !== 'radio') {
+          if (!input.getAttribute('placeholder')) input.setAttribute('placeholder', ' ')
+          inputHandler(input) // force x to appear on inputs with prefilled value
+        }
+
+        if (type !== 'checkbox' && type !== 'radio') {
+          const div = document.createElement('div')
+          div.append(label.closest('dt'), input.closest('dd'))
+          dl.append(div)
+        }
+
+        input.addEventListener('input', inputHandler)
+        input.addEventListener('mousemove', event => {
+          const el = event.target
+
+          if (el.nodeName === 'TEXTAREA' || typeLookup.includes(el.getAttribute('type'))) {
+            inputHandler(event)
+            if (el.offsetWidth - clearfieldHorizontalOffset < event.clientX - el.getBoundingClientRect().left && clearfieldHorizontalOffset + clearfieldVerticalOffset > event.clientY - el.getBoundingClientRect().top
+            ) {
+              if (!el.classList.contains('onX')) {
+                el.classList.add('onX')
               }
-              if (nodeName !== 'SELECT' && type !== 'checkbox' && type !== 'radio') {
-                // if it doesn't have a placeholder, add a blank one
-                if (!input.getAttribute('placeholder')) input.setAttribute('placeholder', ' ')
-                inputHandler(input) // force x to appear on inputs with prefilled value
-              }
-              input.addEventListener('input', inputHandler)
-              input.addEventListener('mousemove', event => {
-                const el = event.target
-                const nodeName = el.nodeName
-                const type = el.getAttribute('type')
-                if (nodeName === 'TEXTAREA' || type === 'checkbox' || type === 'color' || type === 'date' || type === 'datetime-local' || type === 'email' || type === 'file' || type === 'month' || type === 'number' || type === 'password' || type === 'radio' || type === 'range' || type === 'search' || type === 'tel' || type === 'text' || type === 'time' || type === 'url' || type === 'week') {
-                  inputHandler(event)
-                  if (el.offsetWidth - clearfieldHorizontalOffset < event.clientX - el.getBoundingClientRect().left && clearfieldHorizontalOffset + clearfieldVerticalOffset > event.clientY - el.getBoundingClientRect().top
-                  ) {
-                    if (!el.classList.contains('onX')) {
-                      el.classList.add('onX')
-                    }
-                  } else {
-                    el.classList.remove('onX')
-                  }
-                }
-              })
-              input.addEventListener('click', event => {
-                const el = event.target
-                const nodeName = el.nodeName
-                const type = el.getAttribute('type')
-                if (nodeName === 'TEXTAREA' || type === 'color' || type === 'date' || type === 'datetime-local' || type === 'email' || type === 'file' || type === 'month' || type === 'number' || type === 'password' || type === 'search' || type === 'tel' || type === 'text' || type === 'time' || type === 'url' || type === 'week') {
-                  if (el.offsetWidth - clearfieldHorizontalOffset < event.clientX - el.getBoundingClientRect().left &&
-                  clearfieldHorizontalOffset + clearfieldVerticalOffset > event.clientY - el.getBoundingClientRect().top
-                  ) {
-                    el.value = ''
-                    el.dispatchEvent(new Event('input'))
-                    el.form.dispatchEvent(new Event('input'))
-                    el.classList.remove('x')
-                    el.classList.remove('onX')
-                  }
-                }
-              })
+            } else {
+              el.classList.remove('onX')
             }
           }
-        }
+        })
+        input.addEventListener('click', event => {
+          const el = event.target
+          if (el.nodeName === 'TEXTAREA' || typeLookup.includes(el.getAttribute('type'))) {
+            if (el.offsetWidth - clearfieldHorizontalOffset < event.clientX - el.getBoundingClientRect().left &&
+                  clearfieldHorizontalOffset + clearfieldVerticalOffset > event.clientY - el.getBoundingClientRect().top
+            ) {
+              el.value = ''
+              el.dispatchEvent(new Event('input'))
+              el.form.dispatchEvent(new Event('input'))
+              el.classList.remove('x')
+              el.classList.remove('onX')
+            }
+          }
+        })
       }
     }
   }
@@ -96,7 +122,7 @@ window.semanticForms = () => {
     const el = event.target || event
     const nodeName = el.nodeName
     const type = el.getAttribute('type') || nodeName === 'TEXTAREA'
-    if ((nodeName && type) && (nodeName === 'TEXTAREA' || type === 'checkbox' || type === 'color' || type === 'date' || type === 'datetime-local' || type === 'email' || type === 'file' || type === 'month' || type === 'number' || type === 'password' || type === 'radio' || type === 'range' || type === 'search' || type === 'tel' || type === 'text' || type === 'time' || type === 'url' || type === 'week')) {
+    if ((nodeName && type) && (nodeName === 'TEXTAREA' || typeLookup.includes(type))) {
       if (el.value) {
         if (!el.classList.contains('x')) {
           el.classList.add('x')
