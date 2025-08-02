df31dabcc793ec08e92d19d952806c144b9cb728
Eric Newport <kethinov@gmail.com>
2025-08-02 09:06:56 -0400

    fix dev server issues

--- dev-server.js
+++ dev-server.js
@@ -1,4 +1,25 @@
-require('child_process').spawn('sh', ['-c', 'node build.js --development-mode && http-server public/'], {
+const { spawn, spawnSync } = require('child_process')
+
+spawnSync('sh', ['-c', 'npm run build'], {
+  stdio: 'inherit'
+})
+
+spawnSync('sh', ['-c', 'node build.js --development-mode'], {
   stdio: 'inherit',
   cwd: 'docs'
+})
+
+const child = spawn('sh', ['-c', 'http-server public/'], {
+  stdio: 'inherit',
+  cwd: 'docs'
+})
+
+process.on('SIGINT', () => {
+  child.kill('SIGINT')
+  process.exit()
+})
+
+process.on('SIGTERM', () => {
+  child.kill('SIGTERM')
+  process.exit()
 })
